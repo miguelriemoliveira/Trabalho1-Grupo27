445f3d4542b2aa2c35022cbaf60eb0a5128ec829
Joel-Baptista <joelbaptista@ua.pt>
2021-10-28 10:27:31 +0100

    Inicio do projeto

---
+++ main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+def addComplex(x, y):
+    # addapt code to use named tuples
+
+def multiplyComplex(x, y):
+    # addapt code to use named tuples
+
+def printComplex(x):
+    # addapt code to use named tuples
+
+def main():
+    # define two complex numbers as tuples of size two
+
+if __name__ == '__main__':
+    main()
